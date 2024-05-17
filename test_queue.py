f9f89e684bc093f667b6d1e00e66ba12f4e58d13
Alexander Myltsev <a.myltsev@gmail.com>
2024-05-17 19:02:24 +0400

    Make test_queue more future proof.

--- test_queue.py
+++ test_queue.py
@@ -1,24 +1,28 @@
 from bot import DJ
 
 
+def format_next(name, song):
+    return f"Next up: {song} (by {name})\nCommands: /next /listall /remove"
+
+
 def test_queue():
     dj = DJ()
     dj.enqueue(1, "avm", "01")
     dj.enqueue(1, "avm", "03")
     dj.enqueue(2, "alice", "02")
     dj.enqueue(2, "alice", "04")
-    assert dj.next() == "Next up: 01 (by avm)\nCommands: /next /listall /remove"
-    assert dj.next() == "Next up: 02 (by alice)\nCommands: /next /listall /remove"
-    assert dj.next() == "Next up: 03 (by avm)\nCommands: /next /listall /remove"
+    assert dj.next() == format_next("avm", "01")
+    assert dj.next() == format_next("alice", "02")
+    assert dj.next() == format_next("avm", "03")
     dj.enqueue(3, "guest1", "01")
     dj.enqueue(3, "guest1", "02")
-    assert dj.next() == "Next up: 01 (by guest1)\nCommands: /next /listall /remove"
+    assert dj.next() == format_next("guest1", "01")
     dj.enqueue(3, "guest1", "03")
     dj.enqueue(4, "guest2", "04")
-    assert dj.next() == "Next up: 04 (by guest2)\nCommands: /next /listall /remove"
-    assert dj.next() == "Next up: 04 (by alice)\nCommands: /next /listall /remove"
-    assert dj.next() == "Next up: 02 (by guest1)\nCommands: /next /listall /remove"
-    assert dj.next() == "Next up: 03 (by guest1)\nCommands: /next /listall /remove"
+    assert dj.next() == format_next("guest2", "04")
+    assert dj.next() == format_next("alice", "04")
+    assert dj.next() == format_next("guest1", "02")
+    assert dj.next() == format_next("guest1", "03")
     assert dj.next() == "The queue is empty"
 
 
@@ -29,8 +33,8 @@ def test_clear():
     dj.enqueue(2, "alice", "02")
     dj.enqueue(2, "alice", "04")
     dj.clear(1)
-    assert dj.next() == "Next up: 02 (by alice)\nCommands: /next /listall /remove"
-    assert dj.next() == "Next up: 04 (by alice)\nCommands: /next /listall /remove"
+    assert dj.next() == format_next("alice", "02")
+    assert dj.next() == format_next("alice", "04")
 
 
 def test_remove():
@@ -39,7 +43,7 @@ def test_remove():
     dj.enqueue(1, "avm", "03")
     dj.enqueue(2, "alice", "02")
     dj.enqueue(2, "alice", "04")
-    assert dj.next() == "Next up: 01 (by avm)\nCommands: /next /listall /remove"
+    assert dj.next() == format_next("avm", "01")
     assert dj.remove() == "avm removed from the queue"
-    assert dj.next() == "Next up: 02 (by alice)\nCommands: /next /listall /remove"
-    assert dj.next() == "Next up: 04 (by alice)\nCommands: /next /listall /remove"
+    assert dj.next() == format_next("alice", "02")
+    assert dj.next() == format_next("alice", "04")
