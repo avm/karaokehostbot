58edef59a467d9a537d5490efb23bdffc6165d8b
Alexander Myltsev <a.myltsev@gmail.com>
2024-05-24 19:14:45 +0400

    Add test for reloading DJ from the db.

--- test_db.py
+++ test_db.py
@@ -27,7 +27,6 @@ def test_storage():
     dj = DJ(db)
     dj.register(1, "avm")
     dj.enqueue(1, "Elvis")
-    print(db.data)
     assert db["user:1"] == ["Elvis"]
     assert db["new_users"] == [1]
     assert db["names"] == {1: "avm"}
@@ -46,6 +45,46 @@ def test_storage():
     assert db["queue"] == [2]
     assert db["user:1"] == ["Doors"]
     assert db["user:2"] == []
+    dj.remove()
+    assert db["queue"] == []
+    assert "user:2" not in db
+
+
+def test_reload():
+    db = CopyingDict()
+    dj = DJ(db)
+    dj.register(1, "avm")
+    dj.enqueue(1, "Elvis")
+    assert db["user:1"] == ["Elvis"]
+    assert db["new_users"] == [1]
+    assert db["names"] == {1: "avm"}
+    dj.enqueue(2, "Amanda Palmer")
+
+    dj = DJ(db)
+
+    dj.next()  # Elvis
+    assert db["queue"] == [1]
+    dj.next()  # Amanda Palmer
+    assert db["queue"] == [1, 2]
+    assert db["new_users"] == []
+
+    dj = DJ(db)
+
+    dj.register(2, "alice")
+    dj.enqueue(2, "Nickelback")
+    dj.next()  # Nickelback
+    assert db["queue"] == [2]
+    dj.enqueue(1, "Doors")
+
+    dj = DJ(db)
+
+    assert db["new_users"] == [1]
+    assert db["queue"] == [2]
+    assert db["user:1"] == ["Doors"]
+    assert db["user:2"] == []
+
+    dj = DJ(db)
+
     dj.remove()
     assert db["queue"] == []
     assert "user:2" not in db
