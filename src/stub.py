0e6f4e56b43018646a88a37ae53a6055d719a1a6
Alexander Myltsev <a.myltsev@gmail.com>
2024-10-08 18:04:18 +0400

    Add stub.py (dummy responder for the old bot name).

---
+++ src/stub.py
@@ -0,0 +1,40 @@
+from dotenv import load_dotenv
+import os
+import logging
+
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+
+load_dotenv()
+
+TOKEN = os.getenv("OLD_TOKEN")
+
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+# set higher logging level for httpx to avoid all GET and POST requests being logged
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /start is issued."""
+    await update.message.reply_text("Please use @karaokehostbot instead")
+
+
+def main() -> None:
+    """Start the bot."""
+    application = Application.builder().token(TOKEN).build()
+
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("help", start))
+    application.add_handler(MessageHandler(filters.ALL, start))
+
+    # Run the bot until the user presses Ctrl-C
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+
+if __name__ == "__main__":
+    main()
